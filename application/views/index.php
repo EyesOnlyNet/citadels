d75052b918e233c1651999df07097689e9231252
Steve <ice.only@gmx.net>
2013-10-27 01:42:19 +0200

    setup scss-structure

---
+++ application/views/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../styles/css/main.css" type="text/css" />
+        <title>Welcome to Citadels</title>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
