19ab2d90f991bf945718357d47c333f550377d37
Steve <ice.only@gmx.net>
2013-10-27 22:28:04 +0100

    add login page
    * do scss stuff

---
+++ application/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../application/styles/css/main.css" type="text/css" />
+        <link rel="stylesheet" href="../application/styles/css/index.css" type="text/css" />
+        <title>Citadels - the Webversion</title>
+    </head>
+
+    <body>
+        <div class="header">
+            <header>
+                <h1>Ohne Furcht und Adel</h1>
+                <h2>- Webversion -</h2>
+            </header>
+            <hr />
+        </div>
+
+        <form class="welcome" action="check-name.php" method="post">
+            <label>Willkommen Besucher, wie ist dein Name:</label>
+            <input name="user-name" placeholder="Großherzog Augustus Mediacus" />
+            <button value="check">Bauurkunde beantragen</button>
+        </form>
+
+        <div class="footer">
+            <hr />
+            <footer>
+                <ul>
+                    <li><a href="http://www.brettspielwelt.de/Hilfe/Anleitungen/OhneFurchtUndAdel/" target="_blank">How To</a></li>
+                    <li><a href="http://de.wikipedia.org/wiki/Ohne_Furcht_und_Adel" target="_blank">Wikipedia</a></li>
+                    <li>Version: dev-20131027</li>
+                </ul>
+            </footer>
+        </div>
+    </body>
+</html>
