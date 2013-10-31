a427d53c5d7f76f3be8ea6f93682364476b5fe7f
Steve <ice.only@gmx.net>
2013-10-31 01:39:14 +0100

    add charachters

--- application/views/partials/characters.php
+++ application/views/partials/characters.php
@@ -1,8 +1,10 @@
+<link rel="stylesheet" href="../application/styles/css/partials/characters.css" type="text/css" />
+
 <div class="box characters">
     <h4 class="header">Wähle deine Gesinnung</h4>
     <ul class="content character-list">
-        <li class="mch">Meuchler</li>
-        <li class="dib">Dieb</li>
+        <li class="mch not-available out">Meuchler</li>
+        <li class="dib not-available">Dieb</li>
         <li class="mgr">Magier</li>
         <li class="kng">König</li>
         <li class="prd">Prediger</li>
