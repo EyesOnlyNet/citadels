0ba0b30e92bbc8d32394604c9ba743a6a9ab38e4
Steve <ice.only@gmx.net>
2014-09-03 23:38:52 +0200

    * show playerList on result-page

--- src/Citadels/CoreBundle/Resources/public/js/game/results.js
+++ src/Citadels/CoreBundle/Resources/public/js/game/results.js
@@ -1,3 +1,21 @@
 $(function() {
-    
+    var updatePlayerList = function() {
+        var gameId = $('#my-game').data('id');
+
+        $.ajax({
+            url: '/app_dev.php/players/game/' + gameId
+        })
+        .done(function(response) {
+            console.log("updatePlayerList success");
+            console.log($.parseJSON(response));
+
+            var data = { players: $.parseJSON(response).playerList };
+
+            updateViewModel(data, viewModel);
+        })
+        .fail(function() { console.log("updatePlayerList error"); })
+        .always(function() { console.log("updatePlayerList complete"); });
+    };
+
+    updatePlayerList();
 });
