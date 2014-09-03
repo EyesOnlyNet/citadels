478c43eb4397b7acc738fe30b35fe3afa01868e3
Steve <ice.only@gmx.net>
2014-09-04 01:01:12 +0200

    * optimize player-list-view-model

---
+++ src/Citadels/CoreBundle/Resources/public/js/view-model/playerList.js
@@ -0,0 +1,21 @@
+var playerListModel = ko.mapping.fromJS({
+    playerList: [],
+    update: function() {
+        var gameId = $('#my-game').data('id');
+
+        $.ajax({
+            url: '/app_dev.php/players/game/' + gameId
+        })
+        .done(function(response) {
+            console.log("updatePlayerList success");
+            console.log($.parseJSON(response));
+
+            ko.mapping.fromJS(
+                { playerList: $.parseJSON(response).playerList },
+                playerListModel
+            );
+        })
+        .fail(function() { console.log("updatePlayerList error"); })
+        .always(function() { console.log("updatePlayerList complete"); });
+    }
+});
