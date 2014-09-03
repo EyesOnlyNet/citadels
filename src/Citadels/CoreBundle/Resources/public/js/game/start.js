702db2a1587535cab2bb7555ff9dfb31461a5b10
Steve <ice.only@gmx.net>
2014-09-04 01:21:06 +0200

    * optimize my-player-view-model

--- src/Citadels/CoreBundle/Resources/public/js/game/start.js
+++ src/Citadels/CoreBundle/Resources/public/js/game/start.js
@@ -1,43 +1,9 @@
 $(function() {
-    var updateMyPlayer = function() {
-        var fingerprint = new Fingerprint().get(),
-            gameId = $('#my-game').data('id');
+    ko.applyBindings(playerListModel, document.getElementById('player-list'));
+    ko.applyBindings(myPlayerModel, document.getElementById('my-player'));
 
-        $.ajax({
-            url: '/app_dev.php/players/' + fingerprint + '/game/' + gameId
-        })
-        .done(function(response) {
-            console.log("updateMyPlayer success");
-            console.log($.parseJSON(response));
-
-            var data = { myPlayer: $.parseJSON(response).myPlayer };
-
-            updateViewModel(data, viewModel);
-        })
-        .fail(function() { console.log("updateMyPlayer error"); })
-        .always(function() { console.log("updateMyPlayer complete"); });
-    };
-
-    var updatePlayerList = function() {
-        var gameId = $('#my-game').data('id');
-
-        $.ajax({
-            url: '/app_dev.php/players/game/' + gameId
-        })
-        .done(function(response) {
-            console.log("updatePlayerList success");
-            console.log($.parseJSON(response));
-
-            var data = { players: $.parseJSON(response).playerList };
-
-            updateViewModel(data, viewModel);
-        })
-        .fail(function() { console.log("updatePlayerList error"); })
-        .always(function() { console.log("updatePlayerList complete"); });
-    };
-
-    updateMyPlayer();
-    updatePlayerList();
+    playerListModel.update();
+    myPlayerModel.update();
 
     $('#action-tabs').tabs({
         disabled: [],
---
+++ src/Citadels/CoreBundle/Resources/public/js/view-model/myPlayer.js
@@ -0,0 +1,35 @@
+var myPlayerModel = ko.mapping.fromJS({
+    myPlayer: {
+        name: '',
+        gold: 0,
+        buildings: [],
+        handCards: [],
+        points: 0,
+        isActive: false,
+        isKing: false,
+        character: {
+            type: '',
+            name: '',
+            shortcut: ''
+        }
+    },
+    update: function() {
+        var fingerprint = new Fingerprint().get(),
+            gameId = $('#my-game').data('id');
+
+        $.ajax({
+            url: '/app_dev.php/players/' + fingerprint + '/game/' + gameId
+        })
+        .done(function(response) {
+            console.log("updateMyPlayer success");
+            console.log($.parseJSON(response));
+
+            ko.mapping.fromJS(
+                { myPlayer: $.parseJSON(response).myPlayer },
+                myPlayerModel
+            );
+        })
+        .fail(function() { console.log("updateMyPlayer error"); })
+        .always(function() { console.log("updateMyPlayer complete"); });
+    }
+});
